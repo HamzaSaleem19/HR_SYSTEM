2a10a6d0a7b99eb7a7907ae941135aa8e670410a
HamzaSaleem19 <hamzasaleem.hs778@gmail.com>
2023-01-06 11:27:49 +0500

    commit

--- HR_SYSTEM/Models/Department.cs
+++ HR_SYSTEM/Models/Department.cs
@@ -12,7 +12,7 @@ namespace HR_SYSTEM.Models
         public string DepName { get; set; }
         public string LandlineNumber { get; set; }
         public string Location { get; set; }
-        public int CompanyRegNo { get; set; } //DepId Foreign Key
+        public int CompanyRegNo { get; set; } //CompanyRegNo Foreign Key
         [ForeignKey("CompanyRegNo")]
         public Company Company { get; set; }
         public ICollection<Employee> Employees { get; set; }
